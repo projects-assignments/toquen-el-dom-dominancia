3de52dc647304763379067a0bdea714a65e74442
Aina <ainafc3@gmail.com>
2023-12-02 17:29:12 +0100

    updates

---
+++ src/js/guitar.js
@@ -0,0 +1,22 @@
+const guitarStrings = document.querySelectorAll(".guitar-strings .string");
+let allKeys = [];
+const audio = new Audio(`../src/assets/sounds/guitar-sounds/aa.wav`);
+const playTune = (key) => {
+    audio.src = `../src/assets/sounds/guitar-sounds/${key}.wav`;
+    audio.play();
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active");
+    setTimeout(() => {
+        clickedKey.classList.remove("active");
+    }, 150);
+};
+guitarStrings.forEach(key => {
+    allKeys.push(key.dataset.key);
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
+const pressedKey = (e) => {
+    if (allKeys.includes(e.key)) playTune(e.key);
+};
+document.addEventListener("keydown", pressedKey);
+
+
