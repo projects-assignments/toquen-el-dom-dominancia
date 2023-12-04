697afa6eaf6d96abc19c61df99c3cf214816deb0
Aina <ainafc3@gmail.com>
2023-12-04 11:55:51 +0100

    updates

--- src/js/home.js
+++ src/js/home.js
@@ -1,15 +1,28 @@
-const buttons2 = document.getElementsByClassName('button-instruments-home');
-
-for (let i = 0; i < buttons2.length; i++) {
-  buttons2[i].addEventListener('mouseover', function() {
-    this.style.backgroundColor = '#161515'; // Cambiar el color al pasar el cursor por encima
-    this.style.border = '2px solid #D99923'; // Establecer un borde con color
-    this.style.color = '#fff'; // Cambiar el color del texto del botón
+const buttonsHome1 = document.getElementsByClassName('button-try-home');
+for (let i = 0; i < buttonsHome1.length; i++) {
+  buttonsHome1[i].addEventListener('mouseover', function() {
+    this.style.backgroundColor = '#161515';
+    this.style.border = '2px solid #D99923';
+    this.style.color = '#fff';
   });
-  buttons2[i].addEventListener('mouseout', function() {
-    this.style.backgroundColor = '#D99923'; // Restaurar el color original
-    this.style.border = 'none'; // Eliminar el borde al salir
-    this.style.color = 'initial'; // Restaurar el color del texto original
+  buttonsHome1[i].addEventListener('mouseout', function() {
+    this.style.backgroundColor = '#D99923';
+    this.style.border = 'none';
+    this.style.color = 'initial';
+  });
+}
+
+const buttonsHome2 = document.getElementsByClassName('button-instruments-home');
+for (let i = 0; i < buttonsHome2.length; i++) {
+  buttonsHome2[i].addEventListener('mouseover', function() {
+    this.style.backgroundColor = '#161515';
+    this.style.border = '2px solid #D99923';
+    this.style.color = '#fff';
+  });
+  buttonsHome2[i].addEventListener('mouseout', function() {
+    this.style.backgroundColor = '#D99923';
+    this.style.border = 'none';
+    this.style.color = 'initial';
   });
 }
 
